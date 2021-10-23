71fd63972509ba21cb912c74ba3f86c89a7c07b5
unknown <abdelkarim.khalid.m@gmail.com>
2021-10-23 13:45:31 +0200

    display the city name and icon

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let te = document.getElementsByClassName("temp");
 let button = document.querySelector("button");
 let humidity = document.querySelector(".humidity");
 let wind =  document.querySelector(".wind");
+let description = document.querySelector(".description")
+let icons =document.querySelector(".icon")
 button.addEventListener("click", () => {
   let finalArrg = input.value ;
   if (finalArrg) {
@@ -34,8 +36,8 @@ input.addEventListener("keyup", (e) => {
   
 })
 
-function api  (city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=75101a036370a4e5304c153f3b26c5df`)
+function api  (finalArrg) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${finalArrg}&units=metric&appid=75101a036370a4e5304c153f3b26c5df`)
 .then((res)=>{
   let allcitys = res.json();
   
@@ -45,7 +47,9 @@ function api  (city) {
   te[0].innerHTML = Math.ceil(tempofcity.main.temp) +' °C';
   humidity.innerHTML = `Humidity: ${tempofcity.main.humidity}%`
   wind.innerHTML = `Wind speed: ${tempofcity.wind.speed} km/h`
-
+  city[0].innerHTML = `Weather in ${finalArrg}`
+  description.innerHTML = tempofcity.weather[0].description;
+  icons.src  = `https://openweathermap.org/img/wn/${tempofcity.weather[0].icon}.png`
   console.log(tempofcity.main);
 })
 .catch(rej => {
